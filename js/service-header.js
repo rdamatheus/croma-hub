2e9b7029f51a0d2f6f1753f393554f73bc066205
rdamatheus <rdamatheus@gmail.com>
2026-08-21 00:33:40 -0300

    chore: atualiza carregamento do carrinho com Supabase

--- js/service-header.js
+++ js/service-header.js
@@ -1,7 +1,7 @@
 (() => {
   if (!window.CromaCart && !document.querySelector('script[data-croma-cart]')) {
     const s = document.createElement('script');
-    s.src = '/js/cart.js?v=20260817-2';
+    s.src = '/js/cart.js?v=20260821-1';
     s.dataset.cromaCart = '1';
     document.head.appendChild(s);
   }
