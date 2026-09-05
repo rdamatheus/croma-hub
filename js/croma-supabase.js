da58932bbf5a401c4686b3bca89f5749b8ca897d
rdamatheus <rdamatheus@gmail.com>
2026-09-05 17:11:45 -0300

    Move enriquecimento de imagens para cadastro de produtos

--- js/croma-supabase.js
+++ js/croma-supabase.js
@@ -17,13 +17,10 @@ window.__cromaSupabase = supabase;
 const currentPath=location.pathname.replace(/\/+$/,'/');
 if(currentPath==='/interno/produtos/'){
   queueMicrotask(()=>import('/js/catalog-product-enhancer.js?v=20260831-2'));
-  queueMicrotask(()=>import('/js/product-media-inline.js?v=20260831-1'));
+  queueMicrotask(()=>import('/js/product-media-inline.js?v=20260905-2'));
 }
 if(currentPath==='/interno/categorias/'||currentPath==='/interno/segmentos/'){
   queueMicrotask(()=>import('/js/catalog-admin-list-filters.js?v=20260831-1'));
-}
-if(currentPath==='/interno/bling/'){
-  queueMicrotask(()=>import('/js/bling-image-enrichment-ui.js?v=20260905-1'));
 }
 
 export async function getSessionUser(){
