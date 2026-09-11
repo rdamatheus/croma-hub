5048067d525becb31334586a8483ecb5ff87f353
rdamatheus <rdamatheus@gmail.com>
2026-09-10 22:11:25 -0300

    fix: refresh cart script after uploader guard

--- js/service-header.js
+++ js/service-header.js
@@ -6,7 +6,7 @@
   }
 
   if (!window.CromaCart && !document.querySelector('script[data-croma-cart]')) {
-    const s=document.createElement('script');s.src='/js/cart.js?v=20260821-2';s.dataset.cromaCart='1';document.head.appendChild(s);
+    const s=document.createElement('script');s.src='/js/cart.js?v=20260910-1';s.dataset.cromaCart='1';document.head.appendChild(s);
   }
   if (!document.querySelector('script[data-croma-product-gallery]')) {
     const g=document.createElement('script');g.src='/js/product-media-gallery.js?v=20260828-1';g.dataset.cromaProductGallery='1';g.defer=true;document.head.appendChild(g);
